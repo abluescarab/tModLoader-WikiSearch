a86d897d6488a3df17b0c58603fda04cad835974
Alana Gilston <alana@abluescarab.us>
2017-07-10 01:43:18 -0700

    Initial commit.

---
+++ HotKey.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework.Input;
+using Terraria.GameInput;
+using Terraria.ModLoader;
+
+namespace WikiSearch {
+    public class HotKey {
+        private string name;
+        private Keys defaultKey;
+
+        public string Name { get { return name; } }
+        public Keys DefaultKey { get { return defaultKey; } }
+
+        public HotKey(string name, Keys defaultKey) {
+            this.name = name;
+            this.defaultKey = defaultKey;
+        }
+
+        public static string GetTriggerName(Mod mod, string name) {
+            return mod.Name + ": " + name;
+        }
+
+        public static bool JustPressed(Mod mod, string name) {
+            return PlayerInput.Triggers.JustPressed.KeyStatus[GetTriggerName(mod, name)];
+        }
+
+        public static bool JustReleased(Mod mod, string name) {
+            return PlayerInput.Triggers.JustReleased.KeyStatus[GetTriggerName(mod, name)];
+        }
+
+        public bool JustPressed(Mod mod) {
+            return PlayerInput.Triggers.JustPressed.KeyStatus[GetTriggerName(mod, name)];
+        }
+
+        public bool JustReleased(Mod mod) {
+            return PlayerInput.Triggers.JustReleased.KeyStatus[GetTriggerName(mod, name)];
+        }
+    }
+}
---
+++ WikiSearchItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace WikiSearch {
+    class WikiSearchItem : GlobalItem {
+        public override bool Autoload(ref string name) {
+            return true;
+        }
+
+        public override void SetDefaults(Item item) {
+            base.SetDefaults(item);
+            if(item.createTile > -1) {
+                WikiSearch.createTileItems.Add(item);
+            }
+
+            if(item.createWall > 0) {
+                WikiSearch.createWallItems.Add(item);
+            }
+        }
+    }
+}
